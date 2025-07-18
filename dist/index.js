69f36769fc6de42b8516861cbe356d25a3893784
Keviindran Ramachandran <keviinx@yahoo.com>
2025-07-18 11:03:39 +0200

    remove debug messages

--- dist/index.js
+++ dist/index.js
@@ -8176,21 +8176,11 @@ async function run() {
     const accessToken = core.getInput('token');
     const githubAPIUrl = process.env.GITHUB_API_URL || 'https://api.github.com';
 
-    // --- DEBUGGING MESSAGES START ---
-
-    core.info('--- Starting Repository Deletion Action ---');
-    core.info(`Owner: ${owner}`);
-    core.info(`Repository Name: ${repo}`);
-    core.info(`GitHub API URL: ${githubAPIUrl}`);
-
     const octokit = new Octokit({
       auth: accessToken,
       baseUrl: githubAPIUrl
     })
 
-    // Attempt the API request
-    core.info(`Attempting to delete repository: ${owner}/${repo}`);
-    
     await octokit.request('DELETE /repos/{owner}/{repo}', {
       owner,
       repo
@@ -8198,22 +8188,7 @@ async function run() {
 
     core.info('Repository deleted.');
   } catch (error) {
-    // --- ERROR DEBUGGING START ---
-
-    core.error(`An error occurred: ${error.message}`);
-    // Log the entire error object if available for more details
-    if (error.request) {
-      core.error(`Request URL: ${error.request.url}`);
-      core.error(`Request Method: ${error.request.method}`);
-      core.error(`Request Headers: ${JSON.stringify(error.request.headers)}`);
-    }
-    if (error.response) {
-      core.error(`Response Status: ${error.response.status}`);
-      core.error(`Response Headers: ${JSON.stringify(error.response.headers)}`);
-      core.error(`Response Data: ${JSON.stringify(error.response.data)}`);
-    }
-
-    // --- ERROR DEBUGGING END ---
+    core.setFailed(error.message);
   }
 }
 
